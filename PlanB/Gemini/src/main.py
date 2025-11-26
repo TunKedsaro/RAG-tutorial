79ccd645cf801125cc3563b1813fc80aec9573ed
Tun Kedsaro <tun.k@terradigitalventures.com>
2025-11-26 13:39:42 +0700

    update(main.py)x : update main.py

--- PlanB/Gemini/src/main.py
+++ PlanB/Gemini/src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
 from src.gemini_client import analyse_text
 from src.sectionlevel import section_level_analyse
-from pydantic import BaseModel
 
 class AnalyseRequest(BaseModel):
     text: str
@@ -30,16 +31,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# FastAPI structure
 @app.get("/")
-def health():
-    return {"message": "Ok I am ready"}
+def health_Fastapi():
+    print(f"File: main.py -> def health_Fastapi")
+    return {"message": "Ok FastAPI is ready"}
 
-@app.post("/analyse")
-def analyse(payload: AnalyseRequest):
+
+# Health check
+@app.post("/health_Gemini")
+def hellth_Gemini(payload: AnalyseRequest = AnalyseRequest(
+    text="This is Gemini connection test just return 'Gemini API is connected !!!'"
+)):
+    print(f"File: main.py -> def hellth_Gemini")
     response = analyse_text(payload.text)
     return {"message": response}
 
-@app.post("/sectionlevel")
-def analyse(payload: AnalyseRequest):
+
+# API 05 : Criteria 3.0 : Section-Level
+@app.post("/evaluation/sectionlevel")
+def evaluate_section_level(payload: AnalyseRequest):
+    print(f"File: main.py -> def evaluate_section_level")
     response = section_level_analyse(payload.text)
     return {"message": response}
+
